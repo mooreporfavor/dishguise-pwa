bc86d224539c3f51350e07179bd81cf8b80de289
Ryan Moore <mooreporfavor@gmail.com>
2025-12-26 00:33:47 -0500

    feat: add new hero images, background texture, and ConvertKit subscription API endpoint

--- api/subscribe.ts
+++ api/subscribe.ts
@@ -31,7 +31,7 @@ export default async function handler(
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    api_secret: API_SECRET,
+                    api_key: API_SECRET,
                     email: email,
                 }),
             }
